c5859a5b32c4dd25295d5888ca3d29e5cc071f62
GrahamZugPitt <46653792+GrahamZugPitt@users.noreply.github.com>
2020-09-24 18:50:16 -0400

    Add files via upload

--- cs1652-proj1/http_client.c
+++ cs1652-proj1/http_client.c
@@ -22,8 +22,7 @@
 
 #define BUFSIZE 1024
 
-int 
-main(int argc, char ** argv) 
+int main(int argc, char ** argv) 
 {
 
     char * server_name = NULL;
@@ -38,55 +37,96 @@ main(int argc, char ** argv)
         fprintf(stderr, "usage: http_client <hostname> <port> <path>\n");
         exit(-1);
     }
-
-    server_name = argv[1];
-    server_port = atoi(argv[2]);
-    server_path = argv[3];
+	int s;
+	char buf[BUFSIZE];
+	struct hostent *hp;
+	struct sockaddr_in server_address;
+ 	server_name = argv[1];
+	server_port = atoi(argv[2]);
+	server_path = argv[3];
+		server_address.sin_family = AF_INET;
+		server_address.sin_port = htons(server_port);
     
 
-
-    ret = asprintf(&req_str, "GET %s HTTP/1.0\r\n\r\n", server_path);
+    ret = asprintf(&req_str, "GET  /%s HTTP/1.0\r\n\r\n", server_path);
 
     if (ret == -1) {
-        fprintf(stderr, "Failed to allocate request string\n");
+        fprintf(stderr, "Failed to allocate request string.\n");
         exit(-1);
     }
 
-    /*
-     * NULL accesses to quiesce compiler errors
-     * Delete the following lines 
-     */
-    (void)server_name;
-    (void)server_port;
-
     /* make socket */
+	s = socket(AF_INET,SOCK_STREAM, IPPROTO_TCP);
+    	if(s < 0){
+        	fprintf(stderr, "Failed to make socket.\n");
+		return -1; //Error Processing 
+	}
 
     /* get host IP address  */
-    /* Hint: use gethostbyname() */
-
-    /* set address */
-
+	if((hp = gethostbyname(server_name)) == NULL){
+        	fprintf(stderr, "Failed to get host IP address.\n");
+		close(s);
+		return -1; //Error Processing 
+	}
+    /*set address */
+	memcpy(&server_address.sin_addr.s_addr, hp->h_addr, hp->h_length);
     /* connect to the server socket */
-
-    /* send request message */
-
+	if(connect(s, (struct sockaddr *) &server_address, sizeof(server_address)) < 0){
+        	fprintf(stderr, "Failed to connect to host.\n");
+		close(s);	
+		return -1; //Error Processing 
+	}
+    /* send request message */	
+	int res = 0;
+	if((res = write(s,req_str,strlen(req_str))) <= 0){
+        	fprintf(stderr, "Failed to send request to server.\n");
+		close(s);	
+		return -1; //Error Processing 
+	}
+	//printf("%s", req_str); DELETE ME
     /* wait till socket can be read. */
     /* Hint: use select(), and ignore timeout for now. */
-
     /* first read loop -- read headers */
 
-    /* examine return code */   
-
-    //Skip "HTTP/1.0"
-    //remove the '\0'
-
-    // Normal reply has return code 200
+	if((res = read(s,buf,sizeof(buf) - 1)) <= 0){
+        	fprintf(stderr, "Failed to read message from server.\n");
+		close(s);	
+		return -1; //Error Processing 
+	}
+	
+    /* Check for 200 OK */ 	
+	if(strstr(buf, "200 OK") == NULL){	
+		for(int i = 0; i < res; i++)
+			fprintf(stderr, "%c", buf[i]);
+		while((res = read(s,buf,strlen(buf) - 1)) > 0){
+			for(int i = 0; i < res; i++)
+				fprintf(stderr, "%c", buf[i]);
+		}
+		close(s);
+		shutdown(s, 2);
+		return -1;
+	}
 
     /* print first part of response: header, error code, etc. */
-
+	for(int i = 0; i < res; i++)
+		fprintf(stderr, "%c", buf[i]);
+	
     /* second read loop -- print out the rest of the response: real web content */
+	while((res = read(s,buf,strlen(buf) - 1)) > 0){	
+		for(int i = 0; i < res; i++)
+			fprintf(stderr, "%c", buf[i]);
+		if(buf[res-1] == '\n' && buf[res-2] == '\r'){
+			break;
+			}
+	}
+	if(res < 0){
+		fprintf(stderr, "Error occured while reading packets sent from server \n");
+	}
 
     /*close socket and deinitialize */
+	close(s);	
+	shutdown(s, 2);
+	return 0;
 
 
 }
--- cs1652-proj1/http_server1.c
+++ cs1652-proj1/http_server1.c
@@ -13,6 +13,9 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <ctype.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 #include <netinet/in.h>
 #include <arpa/inet.h>
@@ -36,21 +39,62 @@ handle_connection(int sock)
         					"<html><body bgColor=black text=white>\n"               \
         					"<h2>404 FILE NOT FOUND</h2>\n"
         					"</body></html>\n";
-    
-	(void)notok_response;  // DELETE ME
-	(void)ok_response_f;   // DELETE ME
 
     /* first read loop -- get request and headers*/
-    
+	char buf[BUFSIZE];
+	char buf2[sizeof(notok_response)];	
+	read(sock, buf, sizeof(buf) - 1);
     /* parse request to get file name */
     /* Assumption: this is a GET request and filename contains no spaces*/
-
-    /* open and read the file */
+	int pointer = 0;
+	for(int i = 0; i < BUFSIZE; i++){
+		if(buf[i] == '/'){
+			pointer = i+1;
+			for(int j = i; j < BUFSIZE; j++){
+				if(isspace(buf[j]))
+					pointer++;
+				else{
+					break;
+				}
+			}
+		i = BUFSIZE + 1;
+		}
+	}
+	char filename[FILENAMESIZE];
 	
+	for(int i = 0; i < FILENAMESIZE + pointer; i++){
+		if(!isspace(buf[pointer])){
+			filename[i] = buf[pointer++];
+
+		}
+		else{
+			filename[i] = '\0';
+			break;
+		}
+	
+	}
+	printf("%s \n", filename);
+    /* open and read the file */
+	int file = open(filename, 0);
+	int theEndOfTheFile = lseek(file, 0, SEEK_END);
+	sprintf(buf2, ok_response_f, theEndOfTheFile);
+	lseek(file, 0, SEEK_SET);
+	if(file == -1){
+		write(sock, ok_response_f, strlen(notok_response));
+		close(sock);
+		shutdown(sock, 2);
+		return 0;
+		}
+	
+	write(sock, buf2, strlen(buf2));
 	/* send response */
-
+		int track = 0;
+		while((track = read(file, buf, strlen(buf))) > 0){
+			write(sock, buf, track);
+			}
     /* close socket and free pointers */
-
+	close(sock);
+	shutdown(sock, 2);
 	return 0;
 }
 
@@ -59,8 +103,6 @@ int
 main(int argc, char ** argv)
 {
     int server_port = -1;
-    int ret         =  0;
-    int sock        = -1;
 
     /* parse command line args */
     if (argc != 2) {
@@ -77,18 +119,20 @@ main(int argc, char ** argv)
 
     /* initialize and make socket */
 
+	int server_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     /* set server address*/
+	struct sockaddr_in server_address;
+		server_address.sin_family = AF_INET;
+		server_address.sin_port = htons(server_port);
+		server_address.sin_addr.s_addr = INADDR_ANY;
 
     /* bind listening socket */
-
+	bind(server_socket, (struct sockaddr*) &server_address, sizeof(server_address));
     /* start listening */
-
+	listen(server_socket, 10); 
     /* connection handling loop: wait to accept connection */
-
     while (1) {
-        /* handle connections */
-        ret = handle_connection(sock);
-
-		(void)ret; // DELETE ME
+        int client_socket = accept(server_socket, NULL, NULL);
+        handle_connection(client_socket);
     }
 }
