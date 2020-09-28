d926c8c06b393db3e7d01f0d33ea552e6d56b548
tayloor <tak121@pitt.edu>
2020-09-28 12:19:41 -0400

    server 2 implemented

--- cs1652-proj1/http_server2.c
+++ cs1652-proj1/http_server2.c
@@ -1,7 +1,7 @@
 /*
  * CS 1652 Project 1 
  * (c) Jack Lange, 2020
- * (c) <Student names here>
+ * (c) <Graham Zug, Taylor Kveragas>
  * 
  * Computer Science Department
  * University of Pittsburgh
@@ -13,6 +13,9 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <ctype.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 #include <netinet/in.h>
 #include <arpa/inet.h>
@@ -20,6 +23,7 @@
 
 #define BUFSIZE 1024
 #define FILENAMESIZE 100
+#define SERVER_BACKLOG 10
 
 
 static int 
@@ -36,21 +40,77 @@ handle_connection(int sock)
                             "<h2>404 FILE NOT FOUND</h2>\n"				\
                             "</body></html>\n";
     
-  (void)notok_response;  // DELETE ME
-  (void)ok_response_f;   // DELETE ME
+ //(void)notok_response;  // DELETE ME
+ //(void)ok_response_f;   // DELETE ME
 
     /* first read loop -- get request and headers*/
+    char buf[BUFSIZE];
+    char buf2[sizeof(notok_response)]; //Initialize buffers 
+    if(read(sock, buf, sizeof(buf) - 1) < 0){
+            fprintf(stderr, "Failed to read message from client.\n");
+        close(sock);            
+    }
 
     /* parse request to get file name */
+    int pointer = 0;
+    for(int i = 0; i < BUFSIZE; i++){
+        if(buf[i] == '/'){
+            pointer = i+1;
+            for(int j = i; j < BUFSIZE; j++){
+                if(isspace(buf[j]))
+                    pointer++;
+                else{
+                    break;
+                }
+            }
+        i = BUFSIZE + 1;
+        }
+    }
+    char filename[FILENAMESIZE];
+    //Stores file name in buffer
+    for(int i = 0; i < FILENAMESIZE + pointer + 1; i++){
+        if(i == (FILENAMESIZE + pointer)){
+            write(sock, notok_response, strlen(notok_response));
+            close(sock);
+            return 0;           
+        }
+        if(!isspace(buf[pointer])){
+            filename[i] = buf[pointer++];
 
+        }
+        else{
+            filename[i] = '\0';
+            break;
+        }
+    
+    }
     /* Assumption: For this project you only need to handle GET requests and filenames that contain no spaces */
   
     /* open and read the file */
-  
-    /* send response */
+  int file = open(filename, 0);
+    if(file == -1){ //Checks to see that file was opened correctly
+        write(sock, notok_response, strlen(notok_response));
+        close(sock);
+        return -1;
+        }
+    int theEndOfTheFile = lseek(file, 0, SEEK_END); //sets pointer to end of file to get size of file in bytes
+    sprintf(buf2, ok_response_f, theEndOfTheFile); //writes ok response to buffer to we can specify file size
+    lseek(file, 0, SEEK_SET); //resets pointer
+    
+    write(sock, buf2, strlen(buf2));
 
+    /* send response */
+    int track = 0;
+    while((track = read(file, buf, strlen(buf))) > 0){
+        if(track < 0){
+            printf("Error reading file. \n");   
+            close(sock);
+            return -1;
+        }
+        write(sock, buf, track);
+    }
     /* close socket and free space */
-  
+    close(sock);
     return 0;
 }
 
@@ -60,8 +120,8 @@ int
 main(int argc, char ** argv)
 {
     int server_port = -1;
-    int ret         =  0;
-    int sock        = -1;
+    //int ret         =  0;
+    //int sock        = -1;
 
     /* parse command line args */
     if (argc != 2) {
@@ -77,29 +137,80 @@ main(int argc, char ** argv)
     }
     
     /* initialize and make socket */
+    int server_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+        if(server_socket < 0){
+            fprintf(stderr, "Failed to make socket.\n");
+        return -1; //Error Processing 
+    }
 
     /* set server address*/
+    struct sockaddr_in server_address;
+        server_address.sin_family = AF_INET;
+        server_address.sin_port = htons(server_port);
+        server_address.sin_addr.s_addr = INADDR_ANY;
 
     /* bind listening socket */
+    if(bind(server_socket, (struct sockaddr*) &server_address, sizeof(server_address)) < 0){
+            fprintf(stderr, "Failed to bind port.\n");
+        close(server_socket);   
+        return -1; //Error Processing     
+    }
 
     /* start listening */
+        if(listen(server_socket, SERVER_BACKLOG) < 0){ //This means 10 people are allowed to connect (I think?)
+            fprintf(stderr, "Failed to bind port.\n");
+            close(server_socket);
+            return -1; //Error Processing 
+    }
 
     /* connection handling loop: wait to accept connection */
 
+    /* create read list */
+    fd_set curr_socket, temp_socket;
+    FD_ZERO(&curr_socket);
+    FD_ZERO(&temp_socket);
+    FD_SET(server_socket, &curr_socket);
+
+    int max = server_socket;
     while (1) {
+        temp_socket = curr_socket; //select changes fd_set we pass in
     
-        /* create read list */
-        
-        /* do a select */
-        
-        /* process sockets that are ready */
-        
-        /* for the accept socket, add accepted connection to connections */
-        
-        /* for a connection socket, handle the connection */
-        
-        ret = handle_connection(sock);
-        
-        (void)ret;  // DELETE ME
+    /* do a select */
+        if(select(max+1, &temp_socket, NULL, NULL, NULL) < 0){
+            fprintf(stderr, "Select returned an error.\n");
+            close(server_socket);
+            return -1; //Error Processing
+        }
+
+    /* process sockets that are ready */
+        for(int i = 0; i < max+1; i++){
+            if(FD_ISSET(i, &temp_socket)){
+    /* for the accept socket, add accepted connection to connections */
+                if(i == server_socket){
+                    int client_socket = accept(server_socket, NULL, NULL);
+                    if(client_socket < 0){
+                        fprintf(stderr, "Error accepting connection.\n");
+                        close(server_socket);
+                        return -1;
+                    }
+                    if(client_socket > max){
+                        max = client_socket;
+                    }
+                    FD_SET(client_socket, &curr_socket);
+                
+    /* for a connection socket, handle the connection */              
+                }else{
+                    if(handle_connection(i)< 0){
+                        fprintf(stderr, "Error handling connection.\n");
+                        close(server_socket);
+                        return -1; //Error Processing 
+                    }
+                    FD_CLR(i,&curr_socket);
+                }
+            }
+          
+        }
     }
+    return 0;
 }
